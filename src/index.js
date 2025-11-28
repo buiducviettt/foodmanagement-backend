624f77e0887ee2d6bb6916c3582e1b674858be5d
buiducviettt <ducviet8a4@gmail.com>
2025-11-28 16:01:01 +0700

    final x2

--- src/index.js
+++ src/index.js
@@ -1,36 +1,25 @@
-// src/index.js
-
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-
-// giữ nguyên các require cũ
 require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
 const mountRoutes = require('./routes/index.routes.js');
 const { PrismaClient } = require('@prisma/client');
-
 const app = express();
 const prisma = new PrismaClient();
 
-// bật CORS
 app.use(
   cors({
-    origin: 'http://localhost:5173', // cho phép FE gọi
-    credentials: true, // nếu cần gửi cookie/token
+    origin: 'http://localhost:5173',
+    credentials: true,
   }),
 );
-
 app.use(express.json());
 
-// inject prisma vào req
-app.use((req, _res, next) => {
+app.use((req, res, next) => {
   req.prisma = prisma;
   next();
 });
 
-// mount route
 mountRoutes(app);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`🚀 Server running on port ${port}`));
+app.listen(port, () => console.log(`🚀 http://localhost:${port}`));
